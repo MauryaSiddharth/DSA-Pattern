9bd613f9bcd9c3d5c6bdb4b6d3cc2dcc7899a467
Siddharth Maurya <siddharthmaurya7451@gmail.com>
2026-07-29 11:25:01 +0530

    allocate minimum pages gfg

--- binary-search/allocateminimumpages.java
+++ binary-search/allocateminimumpages.java
@@ -1,3 +1,54 @@
 public class allocateminimumpages {
+       //  asked in samsung r&d
     
+    public static void main(String[] args) {
+        int [] arr = {12,34,67,90};
+        int k =2;
+        System.out.println(findPages(arr, k));
+    }
+    static int findPages(int [] arr, int k){
+        int n = arr.length;
+        int res = -1;
+        if(n<k){
+            return -1;
+        }
+        int low = arr[0];
+    for (int pages : arr) {
+        low = Math.max(low, pages);
+    }
+    int high = 0;
+    for (int pages : arr) {
+        high += pages;
+    }
+       while(low<=high){
+        int mid =low + (high - low) / 2;
+        if(fun(arr, k, mid)){
+            res= mid;
+            high = mid-1;
+        }
+        else{
+            low= mid+1;
+        }
+       }
+       return res;
+
+    }
+
+    static boolean fun(int [] arr, int k , int mid ){
+        int student =1;
+        int page = 0;
+        for(int i =0 ; i < arr.length;i++){
+            if(page+arr[i]<=mid){
+                page = page+arr[i];
+            }
+            else{
+                student++;
+                page=arr[i];
+                if(student>k){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
