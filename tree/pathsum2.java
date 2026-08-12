ddb4c0f4385cd65c4a06c324ca3ce6082f3c828a
Siddharth Maurya <siddharthmaurya7451@gmail.com>
2026-08-12 19:43:50 +0530

    path sum 2

--- tree/pathsum2.java
+++ tree/pathsum2.java
@@ -17,20 +17,42 @@ public class pathsum2 {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
-        int target = 3;
+        int targetSum = 3;
         int sum=0;
+        System.out.println(pathSum(root,targetSum));
      }
      static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>>list = new ArrayList<>();
+        List<Integer>list2 = new ArrayList<>();
+        fun(root,targetSum,list2,0,list);
+        return list;
         
     }
 
-     static List<List<Integer>> fun(TreeNode root, int targetSum,List<List<Integer>>list){
-        if(root==null){
-            return null;
-        }
-        list.add(Arrays.asList(root.val));
+     static void fun(TreeNode root, int targetSum,
+                    List<Integer> list2, int sum,
+                    List<List<Integer>> list) {
 
+        if (root == null) {
+            return;
+        }
+
+        sum += root.val;
+        list2.add(root.val);
+
+     
+        if (root.left == null && root.right == null) {
+            if (sum == targetSum) {
+                list.add(new ArrayList<>(list2));
+            }
+        }
+
+       
+        fun(root.left, targetSum, list2, sum, list);
+        fun(root.right, targetSum, list2, sum, list);
+
+    
+        list2.remove(list2.size() - 1);
     }
     
 }
