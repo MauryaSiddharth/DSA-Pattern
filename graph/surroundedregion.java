bd94f58bc01ee50335cb29305870cca29fb784f1
Siddharth Maurya <siddharthmaurya7451@gmail.com>
2026-08-21 23:52:48 +0530

    surrounded region

--- graph/surroundedregion.java
+++ graph/surroundedregion.java
@@ -45,7 +45,7 @@ public class surroundedregion {
             if(board[i][j]=='#'){
                 board[i][j]='O';
             }
-            else{
+             else{
                 board[i][j]='X';
             }
 
